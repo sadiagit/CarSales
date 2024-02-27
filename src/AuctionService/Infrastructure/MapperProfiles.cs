c437dbc88416e486e5da98f3ea281a3e05a9fd5b
Sadia <sadia.boksh@gmail.com>
2024-02-27 22:51:55 +1100

    Carsales commit

---
+++ src/AuctionService/Infrastructure/MapperProfiles.cs
@@ -0,0 +1,16 @@
+﻿using AuctionService.Dtos;
+using AuctionService.Entities;
+using AutoMapper;
+
+namespace AuctionService.Infrastructure;
+
+public class MapperProfiles : Profile
+{
+    public MapperProfiles()
+    {
+        CreateMap<Auction, AuctionDto>().IncludeMembers(x => x.Item);
+        CreateMap<Item, AuctionDto>();
+        CreateMap<CreateAuctionDto, Auction>();
+        CreateMap<CreateAuctionDto, Item>();
+    }
+}
